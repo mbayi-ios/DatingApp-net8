6c3de280441a8c9c2630bd0738fd3f35411ebe07
Ambrose <ambymbayi45@gmail.com>
2024-12-20 14:19:59 +0300

    Add the main photo image to the navbar

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -18,8 +18,7 @@ export class AccountService {
     return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
       map(user => {
         if(user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUser.set(user) 
+          this.setCurrentUser(user);
         }
       })
     )
@@ -29,13 +28,17 @@ export class AccountService {
     return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
       map(user => {
         if(user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUser.set(user) 
+         this.setCurrentUser(user);
         }
 
         return user;
       })
     )
+  }
+
+  setCurrentUser(user: User) {
+    localStorage.setItem('user', JSON.stringify(user));
+    this.currentUser.set(user) 
   }
 
   logout() {
