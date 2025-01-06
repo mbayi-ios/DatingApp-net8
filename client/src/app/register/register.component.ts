973577d98dd773969597e3dab18e50e93cb6fffd
Ambrose <ambymbayi45@gmail.com>
2025-01-06 12:58:51 +0300

    client side registration

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,26 +1,26 @@
 import { Component, inject, input, OnInit, output } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
-import { ToastrService } from 'ngx-toastr';
-import { JsonPipe, NgIf } from '@angular/common';
+import { NgIf } from '@angular/common';
 import { TextInputComponent } from "../_forms/text-input/text-input.component";
 import { DatePickerComponent } from '../_forms/date-picker/date-picker.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule, JsonPipe, NgIf, TextInputComponent, DatePickerComponent],
+  imports: [ReactiveFormsModule, NgIf, TextInputComponent, DatePickerComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent implements OnInit {
   private accountService = inject(AccountService)
   private fb = inject(FormBuilder)
-  private toastr = inject(ToastrService);
   cancelRegister = output<boolean>();
-  model: any = {}; 
+  private router = inject(Router)
   registerForm: FormGroup = new FormGroup({});
   maxDate = new Date();
+  validationErrors: string[] | undefined;
 
   ngOnInit(): void {
     this.initializeForm();
@@ -51,17 +51,21 @@ export class RegisterComponent implements OnInit {
   }
 
   register() {
+    const dob = this.getDateOnly(this.registerForm.get('dateOfBirth')?.value)
+    this.registerForm.patchValue({dateOfBirth: dob})
     console.log(this.registerForm.value)
-    // this.accountService.register(this.model).subscribe({
-    //   next: response => {
-    //     console.log(response);
-    //     this.cancel()
-    //   },
-    //   error: error => this.toastr.error(error.error)
-    // })
+    this.accountService.register(this.registerForm.value).subscribe({
+      next: _ => this.router.navigateByUrl('/members'),
+      error: error => this.validationErrors = error
+    })
   }
 
   cancel() {
     this.cancelRegister.emit(false);
+  }
+
+  private getDateOnly(dob: string | undefined) {
+    if(!dob) return 
+    return new Date(dob).toISOString().slice(0,10)
   }
 }
