a3f4429578dde8035069f90108b55b6078f536cf
Ambrose <ambymbayi45@gmail.com>
2025-01-07 20:25:18 +0300

    Setting up the entities for messaging

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,30 +1,41 @@
 using API.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Data; 
+namespace API.Data;
 
-public class DataContext(DbContextOptions options) : DbContext(options) 
+public class DataContext(DbContextOptions options) : DbContext(options)
 {
-    public DbSet<AppUser> Users {get; set; }
+    public DbSet<AppUser> Users { get; set; }
     public DbSet<UserLike> Likes { get; set; }
+    public DbSet<Message> Messages { get; set; }
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
 
-        builder.Entity<UserLike>() 
-        .HasKey(k => new {k.SourceUserId, k.TargetUserId});
+        builder.Entity<UserLike>()
+        .HasKey(k => new { k.SourceUserId, k.TargetUserId });
 
-        builder.Entity<UserLike>() 
+        builder.Entity<UserLike>()
         .HasOne(s => s.SourceUser)
         .WithMany(l => l.LikedUsers)
         .HasForeignKey(s => s.SourceUserId)
         .OnDelete(DeleteBehavior.Cascade);
 
-        builder.Entity<UserLike>() 
+        builder.Entity<UserLike>()
         .HasOne(s => s.TargetUser)
         .WithMany(l => l.LikedByUsers)
         .HasForeignKey(s => s.TargetUserId)
         .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<Message>()
+        .HasOne(x => x.Recipient)
+        .WithMany(x => x.MessagesReceived)
+        .OnDelete(DeleteBehavior.Restrict);
+
+         builder.Entity<Message>()
+        .HasOne(x => x.Sender)
+        .WithMany(x => x.MessagesSent)
+        .OnDelete(DeleteBehavior.Restrict);
     }
 
 }
