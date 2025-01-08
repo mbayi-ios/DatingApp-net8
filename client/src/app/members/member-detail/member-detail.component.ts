95ad713f93f31707569783c759454be1416dda78
Ambrose <ambymbayi45@gmail.com>
2025-01-08 10:54:35 +0300

    Activating the message tab

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,12 +1,14 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, input, OnInit, ViewChild } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../_models/member';
-import { TabsModule} from 'ngx-bootstrap/tabs';
+import { TabDirective, TabsetComponent, TabsModule } from 'ngx-bootstrap/tabs';
 import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 import { TimeagoModule } from 'ngx-timeago';
 import { DatePipe } from '@angular/common';
 import { MemberMessagesComponent } from "../member-messages/member-messages.component";
+import { Message } from '../../_models/message';
+import { MessageService } from '../../_services/message.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -16,26 +18,39 @@ import { MemberMessagesComponent } from "../member-messages/member-messages.comp
   styleUrl: './member-detail.component.css'
 })
 export class MemberDetailComponent implements OnInit {
+  @ViewChild('memberTabs') memberTabs?: TabsetComponent;
   private memberService = inject(MembersService);
+  private messageService = inject(MessageService)
   private route = inject(ActivatedRoute);
   member?: Member;
   images: GalleryItem[] = [];
+  activeTab?: TabDirective;
+  messages: Message[] = []
 
   ngOnInit(): void {
     this.loadMember()
   }
 
+  onTabActivated(data: TabDirective) {
+    this.activeTab = data;
+    if (this.activeTab.heading === 'Messages' && this.messages.length === 0 && this.member) {
+      this.messageService.getMessageThread(this.member.username).subscribe({
+        next: messages => this.messages = messages
+      })
+    }
+  }
+
   loadMember() {
     const username = this.route.snapshot.paramMap.get('username');
 
-    if(!username) return;
+    if (!username) return;
     this.memberService.getMember(username).subscribe({
       next: member => {
         this.member = member
         member.photos.map(p => {
-          this.images.push(new ImageItem({src: p.url, thumb: p.url}))
+          this.images.push(new ImageItem({ src: p.url, thumb: p.url }))
         })
-      } 
+      }
     })
   }
 
